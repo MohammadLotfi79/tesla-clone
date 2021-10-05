1ecb37bf45db4508435fb7f19f530ef43bd0b571
MohammadLotfi79 <mohammad.lotfi7913@gmail.com>
2021-10-06 00:34:10 +0330

    The project is finished.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
+      {/* This project is done by Mohammad Lotfi */}
       <Header />
       <Home />
     </div>
